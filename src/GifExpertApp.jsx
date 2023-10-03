abaf1d3533f91685289c15973111832c87117a6c
Adal Díaz Fariña <diazfarina.adal@gmail.com>
2023-10-03 12:47:01 +0100

    Gif expert app

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+import { AddCategory, GifGrid } from "./components";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['One Punch', 'Dragon ball'])
+  const {isLoading, setIsLoading} = useState(true);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories]); 
+  }
+  
+  
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+
+      <AddCategory 
+        onNewCategory={ (value) => onAddCategory(value)}
+      />
+
+      <ol>
+        {
+          categories.map(category => (
+            <GifGrid key={category} category={category}/>
+          ))
+        }
+      </ol>
+    </>
+  )
+}
